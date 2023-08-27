c06ab318f7fdb7ce47d134def86fc86b8a87806c
samratghosh291 <samratghosh490@gmail.com>
2023-08-28 01:56:09 +0530

    upload

---
+++ directkeys.py
@@ -0,0 +1,71 @@
+import ctypes
+import time
+
+SendInput = ctypes.windll.user32.SendInput
+
+
+right_pressed = 0x4D
+
+left_pressed = 0x4B
+
+# C struct redefinitions
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+
+class KeyBdInput(ctypes.Structure):
+    fields = [("wVk", ctypes.c_ushort),
+              ("wScan", ctypes.c_ushort),
+              ("dwFlags", ctypes.c_ulong),
+              ("time", ctypes.c_ulong),
+              ("dwExtraInfo", PUL)]
+
+
+class HardwareInput(ctypes.Structure):
+    fields = [("uMsg", ctypes.c_ulong),
+              ("wParamL", ctypes.c_short),
+              ("wParamH", ctypes.c_ushort)]
+
+
+class MouseInput(ctypes.Structure):
+    fields = [("dx", ctypes.c_long),
+              ("dy", ctypes.c_long),
+              ("mouseData", ctypes.c_ulong),
+              ("dwFlags", ctypes.c_ulong),
+              ("time", ctypes.c_ulong),
+              ("dwExtraInfo", PUL)]
+
+
+class Input_I(ctypes.Union):
+    fields = [("ki", KeyBdInput),
+              ("mi", MouseInput),
+              ("hi", HardwareInput)]
+
+
+class Input(ctypes.Structure):
+    fields = [("type", ctypes.c_ulong),
+              ("ii", Input_I)]
+
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002,
+                        0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+if __name__ == '__main_':
+    while (True):
+        PressKey(0x11)
+        time.sleep(1)
+        ReleaseKey(0x11)
+        time.sleep(1)
---
+++ main.py
@@ -0,0 +1,90 @@
+import cv2
+import mediapipe as mp
+import time
+from directkeys import right_pressed, left_pressed
+from directkeys import PressKey, ReleaseKey
+import pyautogui
+
+
+break_key_pressed = left_pressed
+accelerator_key_pressed = right_pressed
+
+time.sleep(2.0)
+current_key_pressed = set()
+
+mp_draw = mp.solutions.drawing_utils
+mp_hand = mp.solutions.hands
+
+tipIds = [4, 8, 12, 16, 20]
+
+video = cv2.VideoCapture(0)
+
+with mp_hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+    while True:
+        keyPressed = False
+        break_pressed = False
+        accelerator_pressed = False
+        key_count = 0
+        key_pressed = 0
+        ret, image = video.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        lmList = []  # Initialize lmList within the loop
+        if results.multi_hand_landmarks:
+            # Only considering the first detected hand
+            hand_landmarks = results.multi_hand_landmarks[0]
+            for id, lm in enumerate(hand_landmarks.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+            mp_draw.draw_landmarks(image, hand_landmarks,
+                                   mp_hand.HAND_CONNECTIONS)
+
+        fingers = []
+        if len(lmList) != 0:
+            if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+            for id in range(1, 5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            total = fingers.count(1)
+
+            if total == 0:
+                print("Brake")
+                pyautogui.keyUp('right')
+                pyautogui.keyDown('left')
+            elif total == 5:
+                print("Gas")
+                pyautogui.keyUp('left')
+                pyautogui.keyDown('right')
+
+        else:
+            print("No hand detected")
+            pyautogui.keyUp('left')
+            pyautogui.keyUp('right')
+
+        if not keyPressed and len(current_key_pressed) != 0:
+            for key in current_key_pressed:
+                ReleaseKey(key)
+            current_key_pressed = set()
+        elif key_count == 1 and len(current_key_pressed) == 2:
+            key_to_release = current_key_pressed.difference(
+                [key_pressed]).pop()
+            ReleaseKey(key_to_release)
+            current_key_pressed = {key_pressed}
+
+        cv2.imshow("Frame", image)
+        k = cv2.waitKey(1)
+        if k == ord('q'):
+            break
+
+video.release()
+cv2.destroyAllWindows()
